270e89f66e8151ebd125e9a08e1681b46e78e38a
Jared Draper <jared@freshlycutwax.com>
2023-04-15 13:34:08 -0500

    Created preprocessor.py

---
+++ preprocessor.py
@@ -0,0 +1,14 @@
+"""
+Preprocessor
+
+Description:
+Script for preprocessing and cleaning package data. Store
+the preprocessed data into a pickle file for mining operations.
+"""
+
+def main():
+	# write some code for main function here
+	
+
+if __name__ == "__main__":
+    main()
