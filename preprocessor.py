874c79fe45d37358b6605cd725756711791712aa
Jared Draper <jared@freshlycutwax.com>
2023-04-15 16:46:18 -0500

    added function for getting data file names

--- preprocessor.py
+++ preprocessor.py
@@ -11,6 +11,22 @@ import pandas as pd
 import sys
 import datetime
 
+from os import listdir
+from os.path import isfile, join
+
+
+def addfiles(path):
+    # get some data filenames
+    filenames = []
+    
+    for file in listdir(path):
+        name = join(path, file)
+        
+        if isfile(name):
+            filenames.append(name)
+            
+    return filenames
+
 
 def parse_date(str_date):
     # convert string date into date object
@@ -26,15 +42,17 @@ def read_data(start_date, end_date):
 def main(args):
 	# command line input date as 'yyyymmdd'
     # first date is begin date, second date is end date
+    data_path = "data/"
+    
     try:
-        begin_date = parse_date(args[1])
+        start_date = parse_date(args[1])
         end_date = parse_date(args[2])
         
-        if begin_date > end_date:
+        if start_date > end_date:
             print("Error: Beginning date is ahead of the end date.")
             sys.exit()
         else:
-            print("Begin Date:", begin_date)
+            print("Start Date:", start_date)
             print("End Date:", end_date)
     except:
         print("An error with the input dates has occurred.")
