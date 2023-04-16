9998dae036489e130d982256b800fc2474e9ba2a
Jared Draper <jared@freshlycutwax.com>
2023-04-15 21:22:22 -0500

    Implemented filename function and added code to get percise dates for the data

--- preprocessor.py
+++ preprocessor.py
@@ -40,22 +40,27 @@ def read_data(start_date, end_date):
 
 
 def main(args):
-	# command line input date as 'yyyymmdd'
-    # first date is begin date, second date is end date
+    # default data path
     data_path = "data/"
+    file_list = []
     
+    # attempt to get the data file list
     try:
-        start_date = parse_date(args[1])
-        end_date = parse_date(args[2])
-        
-        if start_date > end_date:
-            print("Error: Beginning date is ahead of the end date.")
-            sys.exit()
-        else:
-            print("Start Date:", start_date)
-            print("End Date:", end_date)
+        file_list = addfiles(data_path)
     except:
-        print("An error with the input dates has occurred.")
+        print("An error with getting the filenames has occurred.")
+        sys.exit()
+        
+    # attempt to get the range of dates from the files
+    dates = np.full(len(file_list), datetime.date(2000, 1, 1))
+    try:
+        for f in range(len(file_list)):
+            dates[f] = parse_date(file_list[f][13:21])
+            
+        print("Start Date:", dates[0])
+        print("End Date:", dates[len(dates)-1])
+    except:
+        print("An error with getting the dates has occurred.")
         sys.exit()
 	
 
