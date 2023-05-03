bc926368c4bdb8e81658d44f5d4e3b5c7b6976f7
Jared Draper <jared@freshlycutwax.com>
2023-05-02 23:48:44 -0500

    Created the merger script for merging dataframes

---
+++ merger.py
@@ -0,0 +1,20 @@
+"""
+merger
+
+Description: Merge all the dataframes together.
+"""
+
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
