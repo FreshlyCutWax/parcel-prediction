02b8746b0fd2637d3b0b70e083d5276596ed9132
Jared Draper <jared@freshlycutwax.com>
2023-05-03 15:01:52 -0500

    Added history compression to merger script

--- merger.py
+++ merger.py
@@ -8,15 +8,101 @@ import os
 import sys
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
 
 
 
+def compress_history(df_history):
+    # create an array of all our unique package IDs
+    history_idx = pd.unique(df_history['package_id'])
+    
+    # create a list to hold compressed dataframe information
+    history_list = []
+    
+    # days of week dictionary
+    day_dict = {'M':0, 'T':1, 'W':2, 'R':3, 'F':4, 'S':5}
+    
+    for i in tqdm(history_idx):
+        # initialize a list to hold compressed package history
+        pkg_hist = []
+        
+        # get a dataframe of the an individual package history
+        df_pkg = df_history[df_history['package_id'] == i]
+        
+        # append the package ID
+        pkg_hist.append(df_pkg['package_id'].iloc[0])
+        
+        # get the class label
+        pkg_hist.append(df_pkg['status'].iloc[len(df_pkg)-1])
+        
+        # get the days at the station
+        dates = pd.unique(df_pkg['date'])
+        pkg_hist.append(len(dates))
+        
+        # append how many entires the package has
+        pkg_hist.append(max(df_pkg['order'].values)+1)
+        
+        # get the zipcode
+        zips = pd.unique(df_pkg['zipcode'])
+        zips = [x for x in zips if x != 0]
+        try:
+            zipcode = zips[len(zips)-1]
+        except:
+            zipcode = 0
+        pkg_hist.append(zipcode)
+        
+        # get the provider
+        provider = pd.unique(df_pkg['provider'])
+        provider = [x for x in provider if x !='']
+        try:
+            p = provider[len(provider)-1]
+        except:
+            p = 'None'
+        pkg_hist.append(p)
+        
+        # add the area
+        area = pd.unique(df_pkg['assigned_area'])
+        area = [x for x in area if x != 0]
+        try:
+            a = area[len(area)-1]
+        except:
+            a = 0
+        pkg_hist.append(a)
+        
+        # add days of week frequency
+        days = [0, 0, 0, 0, 0, 0]
+        for d in dates:
+            index = df_pkg[df_pkg['date'] == d].index
+            dow = df_history['dow'].iloc[index[0]]
+            days[day_dict[dow]] += 1
+        for i in days:
+            pkg_hist.append(i)
+            
+        # add code frequency
+        s_codes = df_pkg['station_code'].value_counts()
+        if 0 in s_codes:
+            s_codes = s_codes.drop(0)
+        codes = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        for c in s_codes.index:
+            codes[c-1] = s_codes[c]
+        for i in codes:
+            pkg_hist.append(i)
+        
+        # add address issue reason, if any
+        reason = pd.unique(df_pkg['reason'])
+        if reason.any():
+            reason = reason[reason != 0]
+        pkg_hist.append(reason[len(reason)-1])
+        
+        # append to compressed history list
+        history_list.append(pkg_hist)
 
-
-
-
-
-
+    df = pd.DataFrame(history_list, columns=['package_id', 'status', 'days_active', 'actions', 'zipcode', \
+                                             'provider', 'area', 'dow_m', 'dow_t', 'dow_w', 'dow_r', 'dow_f', \
+                                             'dow_s', 'code_1', 'code_2', 'code_3', 'code_4', 'code_5', \
+                                             'code_6', 'code_7', 'code_8', 'code_9', 'address_reason'])
+                                             
+    return df
 
 
 def main():
@@ -53,8 +139,16 @@ def main():
     print("This will take a while. Sit back and grab a drink.")
     print("\n\n")
     input("<Press enter to begin>")
-    # initialize the dataframe
-    df_master = df_package.copy()
+    print("\n\n")
+    
+    # compress the history dataframe
+    print("Compressing History...")
+    df_master = compress_history(df_history)
+    
+    # add package information
+    # df_master = add_package(df_master, df_package)
+    
+    print(df_master)
 
 
 if __name__ == "__main__":
